7366a92a408d4c9ef01976bd3944b63dad47e87b
NyokoDev <129089982+NyokoDev@users.noreply.github.com>
2023-12-02 13:40:11 -0600

    SetValue on Update (Not the best implementation might change in the future)

--- Systems/ModeSystem.cs
+++ Systems/ModeSystem.cs
@@ -14,7 +14,7 @@ using Game;
 using Game.Rendering.CinematicCamera;
 using UnityEngine;
 using Game.Rendering.Utilities;
-
+// The entirety of this code stands as the creation of Nyoko. Any attempt to redistribute or share this code without explicit authorization is strictly prohibited.
 namespace PerformanceSqueezer.Systems
 {
     public partial class ModeSystem : SystemBase
@@ -51,7 +51,7 @@ namespace PerformanceSqueezer.Systems
                 graphicsSettingsApplied = false;
             }
             // Check if PotatoMode is true and ApplyLowestGraphicsSettings() hasn't been applied yet
-            if (PotatoMode && !graphicsSettingsApplied)
+            if (PotatoMode)
             {
                 ApplyLowestGraphicsSettings();
                 graphicsSettingsApplied = true; // Mark settings as applied
@@ -63,40 +63,21 @@ namespace PerformanceSqueezer.Systems
         }
         void ApplyLowestGraphicsSettings()
         {
-
-
             QualitySettings.SetQualityLevel(0, true); // Set to the lowest quality level
-
             QualitySettings.masterTextureLimit = 3; // Reduces texture quality to minimum
-
             QualitySettings.shadows = ShadowQuality.Disable; // Disable shadows
-
             QualitySettings.shadowResolution = ShadowResolution.Low; // Set shadow resolution to low
-
             QualitySettings.shadowDistance = 0; // Set shadow distance to 0
-
             QualitySettings.shadowCascades = 0; // Disable shadow cascades
-
             QualitySettings.shadowProjection = ShadowProjection.CloseFit; // Use close-fit shadow projection
-
             QualitySettings.shadowNearPlaneOffset = 0.2f; // Set shadow near plane offset
-
             QualitySettings.realtimeReflectionProbes = false; // Disable realtime reflection probes
-
             QualitySettings.billboardsFaceCameraPosition = false; // Billboards don't face camera position
-
             QualitySettings.antiAliasing = 0; // Disable anti-aliasing
-
             QualitySettings.asyncUploadTimeSlice = 2; // Set async upload time slice
-
             QualitySettings.asyncUploadBufferSize = 2; // Set async upload buffer size
             SetPrivateFieldValue();
-            SetIsEnabledToTrue();
-            UnityEngine.Debug.Log("Settings set to lowest possible.");
         }
-
-
-
         public void SetPrivateFieldValue()
         {
             Type type = typeof(Game.Rendering.Utilities.AdaptiveDynamicResolutionScale);
@@ -105,47 +86,18 @@ namespace PerformanceSqueezer.Systems
             if (fieldInfo != null)
             {
                 // Setting the value to 0.01f
-                fieldInfo.SetValue(null, 0.5f);
-                UnityEngine.Debug.Log("s_CurrentScaleFraction value set to 0.01f");
+                fieldInfo.SetValue(null, 0.001f);
+               
             }
             else
             {
                 Console.WriteLine("[Critical] CurrentScaleFraction not found.");
             }
         }
-
-        public void SetIsEnabledToTrue()
-        {
-            Type type = typeof(Game.Rendering.Utilities.AdaptiveDynamicResolutionScale); // Replace with your class name
-            PropertyInfo propertyInfo = type.GetProperty("isEnabled");
-
-            if (propertyInfo != null && propertyInfo.CanWrite)
-            {
-                // Setting the value to true
-                propertyInfo.SetValue(null, true, null);
-                UnityEngine.Debug.Log("isEnabled set to true (Enabled)");
-            }
-            else
-            {
-                Console.WriteLine("[Critical] isEnabled property not found or cannot be written.");
-            }
-        }
-        public void SetMinScaleToValue()
-        {
-            Type type = typeof(Game.Rendering.Utilities.AdaptiveDynamicResolutionScale); // Replace with your class name
-            PropertyInfo propertyInfo = type.GetProperty("minScale");
-
-            if (propertyInfo != null && propertyInfo.CanWrite)
-            {
-                // Setting the value to 0.01f
-                propertyInfo.SetValue(null, 0.01f, null);
-                UnityEngine.Debug.Log("minScale value set to 0.01f");
-            }
-            else
-            {
-                Console.WriteLine("[Critical] minScale property not found or cannot be written.");
-            }
-        }
-
     }
 }
+
+
+
+
+// The entirety of this code stands as the creation of Nyoko. Any attempt to redistribute or share this code without explicit authorization is strictly prohibited.
